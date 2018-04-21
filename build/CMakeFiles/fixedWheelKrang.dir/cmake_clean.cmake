084cd40e9662f0104b4dc67f1861a819b60f68c2
mouhyemen <mouhyemen.khan@gmail.com>
2018-04-21 17:55:35 -0400

    First commit for fixed wheel krang ... M. Khan.

---
+++ build/CMakeFiles/fixedWheelKrang.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fixedWheelKrang.dir/Controller.cpp.o"
+  "CMakeFiles/fixedWheelKrang.dir/MyWindow.cpp.o"
+  "CMakeFiles/fixedWheelKrang.dir/Main.cpp.o"
+  "CMakeFiles/fixedWheelKrang.dir/Controller_backup.cpp.o"
+  "fixedWheelKrang.pdb"
+  "fixedWheelKrang"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fixedWheelKrang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
