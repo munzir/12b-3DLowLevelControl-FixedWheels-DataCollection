a6024894974b3c744eafbabaa5246627de2467ed
Krang <you@example.com>
2018-05-01 00:47:13 -0400

    Working but not good looking simulation

--- Controller.cpp
+++ Controller.cpp
@@ -1,38 +1,12 @@
-/*
- * Copyright (c) 2014-2016, Humanoid Lab, Georgia Tech Research Corporation
- * Copyright (c) 2014-2017, Graphics Lab, Georgia Tech Research Corporation
- * Copyright (c) 2016-2017, Personal Robotics Lab, Carnegie Mellon University
- * All rights reserved.
- *
- * This file is provided under the following "BSD-style" License:
- *   Redistribution and use in source and binary forms, with or
- *   without modification, are permitted provided that the following
- *   conditions are met:
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
- *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *   POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include "Controller.hpp"
 #include <nlopt.hpp>
 #include <string>
 #include <iostream>
+#include <dart/dart.hpp>
+
+using namespace dart;
+
+#define DOF 18
 
 //==========================================================================
 Controller::Controller(dart::dynamics::SkeletonPtr _robot,
@@ -60,6 +34,7 @@ Controller::Controller(dart::dynamics::SkeletonPtr _robot,
     mKv(i, i) = 250.0;
   }
 
+
   // Remove position limits
   for (int i = 0; i < dof; ++i)
     _robot->getJoint(i)->setPositionLimitEnforced(false);
@@ -68,33 +43,34 @@ Controller::Controller(dart::dynamics::SkeletonPtr _robot,
   for (int i = 0; i < dof; ++i)
     _robot->getJoint(i)->setDampingCoefficient(0, 0.5);
 
-    // Dump data
-  dataQ.open      ("../data/dataQ.txt");
-  dataQ       << "dataQ" << endl;
 
-  dataQref.open   ("../data/dataQref.txt");
-  dataQref    << "dataQref" << endl;
+  //   // Dump data
+    // dataQ.open      ("../data/dataQ.txt");
+    // dataQ       << "dataQ" << endl;
 
-  dataQdot.open   ("../data/dataQdot.txt");
-  dataQdot    << "dataQdot" << endl;
+    // dataQref.open   ("../data/dataQref.txt");
+    // dataQref    << "dataQref" << endl;
 
-  dataQdotdot.open("../data/dataQdotdot.txt");
-  dataQdotdot << "dataQdotdot" << endl;
+    // dataQdot.open   ("../data/dataQdot.txt");
+    // dataQdot    << "dataQdot" << endl;
 
-  dataTorque.open ("../data/dataTorque.txt");
-  dataTorque  << "dataTorque" << endl;
+    // dataQdotdot.open("../data/dataQdotdot.txt");
+    // dataQdotdot << "dataQdotdot" << endl;
 
-  dataTime.open   ("../data/dataTime.txt");
-  dataTime    << "dataTime" << endl;
+    // dataTorque.open ("../data/dataTorque.txt");
+    // dataTorque  << "dataTorque" << endl;
 
-  dataM.open      ("../data/dataM.txt");
-  dataM       << "dataM" << endl;
+    // dataTime.open   ("../data/dataTime.txt");
+    // dataTime    << "dataTime" << endl;
 
-  dataCg.open     ("../data/dataCg.txt");
-  dataCg      << "dataCg" << endl;
+    // dataM.open      ("../data/dataM.txt");
+    // dataM       << "dataM" << endl;
 
-  dataError.open  ("../data/dataError.txt");
-  dataError   << "dataError" << endl;
+    // dataCg.open     ("../data/dataCg.txt");
+    // dataCg      << "dataCg" << endl;
+
+    // dataError.open  ("../data/dataError.txt");
+    // dataError   << "dataError" << endl;
 }
 
 //=========================================================================
@@ -103,6 +79,11 @@ Controller::~Controller() {}
 struct OptParams {
   Eigen::MatrixXd P;
   Eigen::VectorXd b;
+};
+
+struct comOptParams {
+  dynamics::SkeletonPtr robot;
+  Eigen::Matrix<double, 18, 1> qRefInit;
 };
 
 //=========================================================================
@@ -126,15 +107,64 @@ double optFunc(const std::vector<double> &x, std::vector<double> &grad, void *my
       Eigen::VectorXd::Map(&grad[0], mGrad.size()) = mGrad;
     }
   return (0.5 * pow((optParams->P*X - optParams->b).norm(), 2));
-  }
+}
+//========================================================================
 
-  //==============================================================================
-  template <typename T> int sgn(T val) {
-      return (T(0) < val) - (val < T(0));
-  }
 
-  //==============================================================================
-  Eigen::MatrixXd error(Eigen::VectorXd dq, const int dof) {
+
+//========================================================================
+double comOptFunc(const std::vector<double> &x, std::vector<double> &grad, void *my_func_data) {
+  comOptParams* optParams = reinterpret_cast<comOptParams *>(my_func_data);
+  Eigen::Matrix<double, 18, 1> q(x.data());
+  optParams->robot->setPositions(q);
+
+  if (!grad.empty()) {
+    Eigen::Matrix<double, 1, 18> mGrad = 2*optParams->robot->getCOM()(2)*optParams->robot->getCOMLinearJacobian().block<1, 18>(2,0); 
+    Eigen::VectorXd::Map(&grad[0], mGrad.size()) = mGrad;
+  }
+  
+  
+  return (pow(optParams->robot->getCOM()(2), 2)); 
+
+}
+//==============================================================================
+
+
+//========================================================================
+void constraintFunc(unsigned m, double *result, unsigned n, const double* x, double* grad, void* f_data) {
+  OptParams* constParams = reinterpret_cast<OptParams *>(f_data);
+  //std::cout << "done reading optParams " << std::endl;
+  
+  if (grad != NULL) {
+    for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++){
+        grad[i*n+j] = constParams->P(i, j);
+      }
+    }
+  }
+  // std::cout << "done with gradient" << std::endl;
+
+  Eigen::Matrix<double, 18, 1> X;
+  for(size_t i=0; i<n; i++) X(i) = x[i];
+  //std::cout << "done reading x" << std::endl;
+  
+  Eigen::VectorXd mResult;
+  mResult = constParams->P*X - constParams->b;
+  for(size_t i=0; i<m; i++) {
+    result[i] = mResult(i);
+  }
+  // std::cout << "done calculating the result"
+}
+//==============================================================================
+
+
+//==============================================================================
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
+//==============================================================================
+Eigen::MatrixXd error(Eigen::VectorXd dq, const int dof) {
   Eigen::Matrix<double, 18, 1> alpha;
   for (int i = 0; i < dof; i++) {
     double sign = sgn(dq(i));
@@ -153,15 +183,14 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
 
   // Define coefficients for sinusoidal, pulsation frequency for q and dq
   double wf = 0.558048373585;
-  // double wf = 0.95;
+
+  // base, waist, torso, kinect, left[1-7], right [1-7]
   Eigen::Matrix<double, 18, 4> a, b;
-  a << -0.009, -0.36, 0.311, -0.362,
+  a <<  0.009, -0.36, 0.311, -0.362,
         0.095, -0.132, -0.363, 0.474,
         -0.418, -0.25, -0.12, 0.119,
         0.023, 0.113, 0.497, 0.213,
-        -0.23, -0.237, 0.153, -0.147,
-        0.366, 0.366, 0.302, -0.373,
-        -0.247, -0.166, 0.315, 0.031,
+
         -0.009, -0.36, 0.311, -0.362,
         0.095, -0.132, -0.363, 0.474,
         -0.418, -0.25, -0.12, 0.119,
@@ -169,18 +198,22 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
         -0.23, -0.237, 0.153, -0.147,
         0.366, 0.366, 0.302, -0.373,
         -0.247, -0.166, 0.315, 0.031,
-        0.366, 0.366, 0.302, -0.373,
-        -0.247, -0.166, 0.315, 0.031,
-        -0.009, -0.36, 0.311, -0.362,
-        0.095, -0.132, -0.363, 0.474;
+        
+        0.206, 0.322, 0.386, 0.386,
+        0.092, 0.186, 0.386, 0.386,
+        0.386, 0.386, 0.386, -0.043,
+        0.296, 0.262, 0.062, -0.043,
+        0.386, 0.262, 0.062, 0.062,
+        0.368, 0.186, 0.262, 0.186,
+        0.262, 0.386, 0.28, 0.28;
+
+
 
   b <<  -0.051, 0.027, 0.003, -0.332,
         -0.292, 0.358, -0.056, -0.436,
         -0.355, 0.039, -0.397, -0.445,
         0.328, 0.256, -0.36, 0.143,
-        0.428, 0.093, 0.035, -0.28,
-        -0.39, -0.085, 0.388, 0.46,
-        -0.046, 0.135, -0.428, 0.387,
+
         -0.051, 0.027, 0.003, -0.332,
         -0.292, 0.358, -0.056, -0.436,
         -0.355, 0.039, -0.397, -0.445,
@@ -188,15 +221,22 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
         0.428, 0.093, 0.035, -0.28,
         -0.39, -0.085, 0.388, 0.46,
         -0.046, 0.135, -0.428, 0.387,
-        -0.39, -0.085, 0.388, 0.46,
-        -0.046, 0.135, -0.428, 0.387,
-        -0.046, 0.135, -0.428, 0.387,
-        -0.051, 0.027, 0.003, -0.332;
-        
-  Eigen::Matrix<double, 18, 1> q0;
-  q0 << 0.235, -0.004, -0.071, 0.095, -0.141, 0.208, -0.182, 0.095, 0.096,
-        0.071, -0.004, -0.071, 0.095, -0.141, 0.208, -0.182, 0.095, 0.096;
+
+        -0.031, 0.386, 0.386, 0.386,
+        0.298, 0.386, 0.386, 0.386,
+        0.386, 0.154, 0.08, 0.08,
+        0.231, 0.372, 0.367, 0.162,
+        0.386, -0.043, -0.043, 0.262,
+        0.319, -0.031, 0.262, -0.043,
+        0.386, -0.043, 0.22, 0.18;
   
+
+            // -0.004
+
+  Eigen::Matrix<double, 18, 1> q0;
+  q0 << 0.235, M_PI/3, -0.071, 0.095, 
+        0.235, -0.004, -0.071, 0.095, -0.141, 0.208, -0.182, 
+        -0.187, 0.085, -0.09, -0.014, -0.053, 0.187, 0.066;  
 
   // Compute joint angles & velocities using Pulsed Trajectories
   Eigen::Matrix<double, 18, 1> qref;
@@ -206,7 +246,7 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
   dqref = qref;
 
   cout << "Time: " << mTime << endl;
-  for (int joint = 0; joint < dof; joint++) {
+  for (int joint = 1; joint < dof; joint++) {
     for (int l = 1; l <= 4; l++) {
 
       qref(joint) = qref(joint) + (a(joint, l-1)/(wf*l))*sin(wf*l*mTime)
@@ -219,8 +259,46 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
   }
 
   qref = qref + q0;
+  qref(0) = qref_vec[0];
+
+  comOptParams comParams;
+  comParams.robot = mRobot;
+  comParams.qRefInit << qref; 
+
+  OptParams constraintOptParams;
+  Eigen::Matrix<double, 18, 18> P_q0 = Eigen::Matrix<double, 18, 18>::Identity();
+  Eigen::Matrix<double, 18, 1>  b_q0;
+
+  P_q0(0,0) = 0.0;
+  b_q0 << qref;
+  b_q0(0) = 0.0;
+
+  constraintOptParams.P = P_q0;
+  constraintOptParams.b = b_q0;
+
+  nlopt::opt opt(nlopt::LN_COBYLA, dof);
+  Eigen::VectorXd::Map(&qref_vec[0], qref.size()) = qref;
+  double minf;
+
+  double prev_qref0 = qref_vec[0];
+  // for (std::vector<double>::const_iterator i = qref_vec.begin(); i != qref_vec.end(); ++i)
+  //   std::cout << *i << ' ';
+
+  opt.set_min_objective(comOptFunc, &comParams);
+  std::vector<double> constraintTol(18, 1e-8);
+  opt.add_equality_mconstraint(constraintFunc, &constraintOptParams, constraintTol);
+
+  opt.set_xtol_rel(1e-4);
+
+  opt.set_maxtime(10);
+
+  opt.optimize(qref_vec, minf);
 
   mTime += dt;
+
+  qref(0) = qref_vec[0];
+
+  dqref(0) = (qref_vec[0] - prev_qref0)/dt;
 
   // Get the stuff that we need
   Eigen::MatrixXd  M    = mRobot->getMassMatrix();                // n x n
@@ -231,10 +309,9 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
   Eigen::VectorXd ddqref = -mKp*(q - qref) - mKv*(dq - dqref);    // n x 1
 
   // Optimizer stuff
-  nlopt::opt opt(nlopt::LD_MMA, 18);
+  nlopt::opt opt_qp(nlopt::LD_MMA, 18);
   OptParams optParams;
   std::vector<double> ddqref_vec(18);
-  double minf;
   // cout << "Initialized optimizer variables ... " << endl << endl;
 
   // Perform optimization to find joint accelerations
@@ -245,36 +322,36 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
   optParams.b = ddqref;
   // cout << "Success !" << endl << endl;
 
-  opt.set_min_objective(optFunc, &optParams);
-  opt.set_xtol_rel(1e-4);
-  opt.set_maxtime(0.005);
-  opt.optimize(ddqref_vec, minf);
+  opt_qp.set_min_objective(optFunc, &optParams);
+  opt_qp.set_xtol_rel(1e-4);
+  opt_qp.set_maxtime(0.005);
+  opt_qp.optimize(ddqref_vec, minf);
   Eigen::Matrix<double, 18, 1> ddqRef(ddqref_vec.data());
  
 
  //torques
   mForces = M*ddqRef + Cg;
   Eigen::Matrix<double, 18, 18> errCoeff = Eigen::Matrix<double, 18, 18>::Identity();
-  errCoeff(0,0) = 	200.0;
-  errCoeff(1,1) = 	500.0;
-  errCoeff(2,2) = 	500.0;
-  errCoeff(3,3) = 	30.0;
+  errCoeff(0,0) = 	1.0;
+  errCoeff(1,1) = 	1.0;
+  errCoeff(2,2) = 	1.0;
+  errCoeff(3,3) = 	1.0;
   errCoeff(4,4) =  	1.0;
 
-  errCoeff(5,5) = 	30.0;
-  errCoeff(6,6) =  	5.0;
-  errCoeff(7,7) = 	15.0;
-  errCoeff(8,8) = 	0.3;
-  errCoeff(9,9) = 	25.0;
-  errCoeff(10,10) = 5.0;
+  errCoeff(5,5) = 	1.0;
+  errCoeff(6,6) =  	1.0;
+  errCoeff(7,7) = 	1.0;
+  errCoeff(8,8) = 	1.3;
+  errCoeff(9,9) = 	1.0;
+  errCoeff(10,10) = 1.0;
 
-  errCoeff(11,11) = 15.0;
-  errCoeff(12,12) = 75.0;
-  errCoeff(13,13) = 4.0;
-  errCoeff(14,14) = 15.0;
-  errCoeff(15,15) = 0.5;
-  errCoeff(16,16) = 10.0;
-  errCoeff(17,17) = 15.0;
+  errCoeff(11,11) = 1.0;
+  errCoeff(12,12) = 1.0;
+  errCoeff(13,13) = 1.0;
+  errCoeff(14,14) = 1.0;
+  errCoeff(15,15) = 1.5;
+  errCoeff(16,16) = 1.0;
+  errCoeff(17,17) = 1.0;
 
 
   Eigen::VectorXd mForceErr = mForces + errCoeff*error(dq, dof);
@@ -294,18 +371,18 @@ void Controller::update(const Eigen::Vector3d& _targetPosition) {
 
   // Closing operation
   double T = 2*3.1416/wf;
-  if (mTime >= 2*T ) {
+  if (mTime >= 1*T ) {
     cout << "Time period met. Stopping data recording ...";
-    dataQ.close();
-    dataQref.close();
-    dataQdot.close();
-    dataQdotdot.close();
-    dataTorque.close();
-    dataTime.close();
-    dataM.close();
-    dataCg.close();
-    dataError.close();
-    cout << "File handles closed!" << endl << endl << endl;
+    // dataQ.close();
+    // dataQref.close();
+    // dataQdot.close();
+    // dataQdotdot.close();
+    // dataTorque.close();
+    // dataTime.close();
+    // dataM.close();
+    // dataCg.close();
+    // dataError.close();
+    // cout << "File handles closed!" << endl << endl << endl;
     exit (EXIT_FAILURE);
   }  
 }
